83e1082b59849a2806fe43a08d2aab289af579da
Lucy Lima Santos <lucyatmosphere@gmail.com>
2024-07-19 20:54:42 +0000

    fix: artists agora é um array de strings

--- src/controller/artist.controller.js
+++ src/controller/artist.controller.js
@@ -1,16 +1,22 @@
 const artists = [
-  {
-    artist: '7038634357'
-  },
-  {
-    artist: 'lucy eris'
-  }
-]
+  "7038634357",
+  "lucy eris",
+  "Caroline Polachek",
+  "Mitski",
+  "Malibu",
+  "Ecco2k",
+  "Bladee",
+  "Chico Buarque",
+  "Yeule",
+  "Lady Gaga",
+  "Melanie Martinez",
+  "Amaarae",
+];
 
 const randomArtist = () => {
-  const random = Math.floor(Math.random() * artists.length)
-  const response = artists[random]
-  return response
-}
+  const random = Math.floor(Math.random() * artists.length);
+  const response = artists[random];
+  return response 
+};
 
-export { randomArtist }
+export { randomArtist };
